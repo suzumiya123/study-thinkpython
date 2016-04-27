9de6c92e22ed15a2635f6f0b6945846e9a1641ca
daiwu_ubuntu <daiwu@ubuntu.com>
2016-04-27 21:55:22 +0800

    add a list 'abc'

--- the_list.py
+++ the_list.py
@@ -4,11 +4,14 @@
 L = [
 	['Apple', 'Google','Microsoft'],
 	['Java', 'Python', 'Ruby', 'PHP'],
-	['Adam', 'Part', 'Lisa']
- ]
+	['Adam', 'Part', 'Lisa'],
+	'abc' 
+]
 #打印 Apple:
 print(L[0][0])
 #打印Python:
 print(L[1][1])
 #打印Lisa:
 print(L[2][2])
+#打印something：
+print(L[3])
