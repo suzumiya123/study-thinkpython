79618dd02ba4b109c36b1b28df49bcbf1df122b1
daiwu_ubuntu <daiwu@ubuntu.com>
2016-04-27 23:28:10 +0800

    add the do_for.py

---
+++ do_for.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- conding: utf-8 -*-
+
+sum = 0
+for x in range(101):
+	sum = sum + x
+print(sum)
+
+aum = 0
+a = 100
+while a >=0:
+	aum  = aum + a
+	a= a -1
+print(aum)
+
