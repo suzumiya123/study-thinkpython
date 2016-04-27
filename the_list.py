a8255898e29976efda7fa7b961a422836d8506e2
daiwu_ubuntu <daiwu@ubuntu.com>
2016-04-27 21:50:53 +0800

    how to use list

---
+++ the_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+L = [
+	['Apple', 'Google','Microsoft'],
+	['Java', 'Python', 'Ruby', 'PHP'],
+	['Adam', 'Part', 'Lisa']
+ ]
+#打印 Apple:
+print(L[0][0])
+#打印Python:
+print(L[1][1])
+#打印Lisa:
+print(L[2][2])
